1ac37241244cfd5a888faf254eb573c683851382
Jing Wang <jwang2048@gmail.com>
2017-05-18 22:42:56 -0400

    add lyrics to sidebar

--- browser/react/index.js
+++ browser/react/index.js
@@ -9,10 +9,15 @@ import Artist from './components/Artist';
 import Songs from './components/Songs';
 import NewPlaylistContainer from './containers/NewPlaylistContainer';
 import Playlist from './components/Playlist';
+import store from './store';
+import {setLyrics} from './action-creators/lyrics';
+import LyricsContainer from './containers/LyricsContainer';
+
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path='/' component={AppContainer} foo={'foo'}>
+      <Route path="/lyrics" component={LyricsContainer} />
       <Route path="/albums" component={Albums} />
       <Route path="/albums/:albumId" component={Album} />
       <Route path="/artists" component={FilterableArtistsContainer} />
@@ -28,35 +33,8 @@ ReactDOM.render(
   document.getElementById('app')
 );
 
-// import store from './store';
-// import {setLyrics} from './action-creators/lyrics';
 
-// console.log('-------------------------');
-// console.log('State before any actions: ', store.getState());
 
-// const inTheAirTonightAction = setLyrics('I can feel it coming in the air tonight ... hold on ...');
-// store.dispatch(inTheAirTonightAction);
 
-// console.log('-------------------------');
-// console.log('State after first SET_LYRICS action: ', store.getState());
 
-// const rickRollAction = setLyrics('Never gonna give you up, never gonna let you down');
-// store.dispatch(rickRollAction);
 
-// console.log('-------------------------');
-// console.log('State after second SET_LYRICS action: ', store.getState());
-
-import store from './store';
-import {setLyrics} from './action-creators/lyrics';
-
-const unsubscribe = store.subscribe(function () {
-    console.log('----------------');
-    console.log('State changed!!', store.getState());
-});
-
-store.dispatch(setLyrics('I can feel it coming in the air tonight ... hold on ...'));
-store.dispatch(setLyrics('Never gonna give you up, never gonna let you down'));
-
-unsubscribe();
-
-store.dispatch(setLyrics('Hello, darkness, my old friend.'));
